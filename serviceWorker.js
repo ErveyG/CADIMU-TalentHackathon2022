b96a52e587281bb0e558427e04c5b3ac3c52334f
Martin Otamendi <89677633+MartinOtamendiT@users.noreply.github.com>
2022-07-22 13:27:43 -0500

    Update SW

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,8 +5,8 @@ const cacheCadimu = 'CADIMU-site-v1',
     'https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
     "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap",
     "https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap",
-    './pokestyles.css',
-    './pokedex.js',
+    './public/playground_assets',
+    './index.js',
     './icons/CADIMU_Icon0.png'
   ]
 
