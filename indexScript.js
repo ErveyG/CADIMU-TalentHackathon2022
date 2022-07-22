9234befce8f6b51f4eefd502f932d0e66e8d2476
Ervey Guerrero <69262706+ErveyG@users.noreply.github.com>
2022-07-22 12:19:48 -0500

    Delete indexScript.js

--- indexScript.js
+++
@@ -1,6 +0,0 @@
-//Verfica que el service Worker exista y lo registra.
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./serviceWorker.js')
-      .then(reg => console.log('Service Worker registrado', reg))
-      .catch(err => console.warn('Error al registrar Service Worker', err))
-}
